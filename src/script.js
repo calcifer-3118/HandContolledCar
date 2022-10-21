927b81ccde6fba321fc24476a565af7a5a2d30d0
calcifer-3118 <dheerjain5002000@gmail.com>
2022-10-21 18:06:24 +0530

    updated UI

--- src/script.js
+++ src/script.js
@@ -1,6 +1,4 @@
-import './style.css'
 import './home.css'
-import './slide1691.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
